5f5984149fdddd4bdab20626461ea5bf1e6b6850
ohihateit <whoamins33@gmail.com>
2023-01-20 12:15:47 +0300

    Initial commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet var mainView: UIView!
+    let ballArray = [
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5")]
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        let randomNumber = Int.random(in: 0 ... 4)
+        imageView.image = ballArray[randomNumber]
+        if randomNumber == 1 {
+            mainView.backgroundColor = UIColor.red
+        }
+        
+        switch randomNumber {
+            case 0:
+            mainView.backgroundColor = UIColor.green
+            case 1:
+                mainView.backgroundColor = UIColor.red
+            default:
+            mainView.backgroundColor = hexStringToUIColor(hex: "#62B7F7")
+            mainView.tintColor = UIColor.systemCyan
+        }
+    }
+    
+    private func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
 
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
 
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
 
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+        
+    }
 }
 
